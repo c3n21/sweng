dd9a71aaa13d7cf7041c3ff7908e0c744050e0fb
Zhou Xu zhou <zepoli.zeme.hotmail@gmail.com>
2021-10-31 16:26:48 +0100

    refactor(User): l'ho appena creata

---
+++ sweng-app/src/main/java/users/User.java
@@ -0,0 +1,22 @@
+package users;
+
+public class User {
+	private String name;
+	private String surname;
+	
+	public User(String n, String s) {
+		name = n;
+		surname = s;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+
+
+	public String getSurname() {
+		return surname;
+	}
+
+}
