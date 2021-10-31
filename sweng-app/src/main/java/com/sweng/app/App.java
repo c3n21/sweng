e574dcc65b6329f094f379642b80c16acf9caa59
Zhou Xu zhou <zepoli.zeme.hotmail@gmail.com>
2021-10-31 15:46:20 +0100

    viva la germania

--- sweng-app/src/main/java/com/sweng/app/App.java
+++ sweng-app/src/main/java/com/sweng/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Je suis Filippò" );
+        System.out.println( "Hello World! Je suis Filippò Ich bin Fliedrich" );
     }
 
 }
