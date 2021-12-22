09103295828f11c5ce3ce87a00ff75cee81b74ad
c3n21 <me@zhifan.me>
2021-12-22 18:18:42 +0100

    test(dao): aggiunti test

---
+++ sweng-app/src/test/java/dao/TestImpiegatoDao.java
@@ -0,0 +1,27 @@
+package dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import utente.Impiegato;
+
+/**
+ * Unit test for {@link ConfigurationManager}
+ */
+public class TestImpiegatoDao {
+
+    @Test
+    public void testSaveAndGet() throws DaoGenericException
+    {
+        Impiegato test = new Impiegato(-1234, "Test", "Impiegato", "password");
+        ImpiegatoDao dao = new ImpiegatoDao();
+        String[] params = {"Test", "Impiegato", "password"};
+        dao.save(test);
+
+        List<Impiegato> impiegati = dao.get(params);
+        assertEquals(1, impiegati.size(), "Più di un impiegato");
+    }
+}
---
+++ sweng-app/src/test/java/dao/TestUtenteDao.java
@@ -0,0 +1,39 @@
+package dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import utente.Sesso;
+import utente.Utente;
+
+/**
+ * Unit test for {@link ConfigurationManager}
+ */
+public class TestUtenteDao {
+
+
+    @Test
+    public void testSaveAndGet() throws DaoGenericException
+    {
+        Utente test = new Utente(
+            -1234, "Test", "Utente",
+
+            Sesso.MASCHIO,
+            LocalDate.now(),
+            "Comune",
+            "Nazione",
+            "ASKDJLHAJKSLFHHH",
+            "password"
+        );
+        UtenteDao dao = new UtenteDao();
+        String[] params = {"Test", "Utente", "password"};
+        dao.save(test);
+
+        List<Utente> utenti = dao.get(params);
+        assertEquals(1, utenti.size(), "Più di un utente");
+    }
+}
