baee0c8ee406483e23966b069bc3a949f1ca6bdf
c3n21 <me@zhifan.me>
2021-12-22 18:23:03 +0100

    feat(Encryption): implementazione sha512

---
+++ sweng-app/src/main/java/utils/Encryption.java
@@ -0,0 +1,46 @@
+package utils;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Encryption
+ *
+ * Adattato da:
+ * 
+ * @see <a href="https://www.geeksforgeeks.org/sha-512-hash-in-java/">https://www.geeksforgeeks.org/sha-512-hash-in-java/</a>
+ */
+public class Encryption {
+
+    public static String sha512(String input) {
+        try {
+            // getInstance() method is called with algorithm SHA-512
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+
+            // digest() method is called
+            // to calculate message digest of the input string
+            // returned as array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+
+            // Add preceding 0s to make it 32 bit
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+            // return the HashText
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
